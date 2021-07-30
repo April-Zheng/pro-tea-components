ed49d6ffa56efeb16b374933f009854805a08be0
zlp <1131808958@qq.com>
2021-07-30 17:58:54 +0800

    ✨ feat: 修改icon。

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,8 @@ const PackageJson = require('./package.json');
 
 export default defineConfig({
   title: 'pro-tea-components',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/assets/icon.ico',
+  logo: '/assets/logo.svg',
   outputPath: 'docs-dist',
   base: process.env.NODE_ENV === 'production' ? `/${PackageJson.name}` : '/',
   publicPath:
