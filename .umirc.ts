8b8c56916d739fbe2d12b9e3704e4f58c673e951
zlp <1131808958@qq.com>
2021-07-30 12:03:35 +0800

    🐞 fix: fix github-page样式失效和public_path问题

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'dumi';
-const PackageJson = require('./package.json')
+const PackageJson = require('./package.json');
 
 export default defineConfig({
   title: 'pro-tea-components',
@@ -7,8 +7,15 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  base: process.env.NODE_ENV === 'production' ? `/${PackageJson.name}` : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? `/${PackageJson.name}/` : '/',
   mfsu: {},
+  manifest: {},
+  hash: true,
+
   sass: {
+    implementation: require('node-sass'),
     prependData: `@import "~@/styles/index.scss";`,
   },
   // more config: https://d.umijs.org/config
